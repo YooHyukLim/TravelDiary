9e2159ffbc6ffe80a50d91711c51797069d7a9e0
CRUD004 <lhb423@naver.com>
2015-06-03 02:47:47 +0900

    hanbin test

--- app/src/main/java/com/example/y/travel_diary/MainActivity.java
+++ app/src/main/java/com/example/y/travel_diary/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+//hanbin checked
+//god yoohyuk bless me
 
 public class MainActivity extends ActionBarActivity {
 
