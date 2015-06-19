0b09a551ce9b271da94e0b589c2ab04bf28ca0ac
YooHyukLim <dbgur3065@gmail.com>
2015-06-19 17:12:59 +0900

    AddNewTravel layout 추가

--- app/src/main/java/com/example/y/travel_diary/Activities/AddNewTravel.java
+++ app/src/main/java/com/example/y/travel_diary/Activities/AddNewTravel.java
@@ -3,10 +3,13 @@ package com.example.y.travel_diary.Activities;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.y.travel_diary.R;
+
 public class AddNewTravel extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
