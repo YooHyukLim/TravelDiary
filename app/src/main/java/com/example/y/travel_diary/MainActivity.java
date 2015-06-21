2e7b5a2f3542acbe3076659e8517db3b2933c43f
YooHyukLim <dbgur3065@gmail.com>
2015-06-21 21:30:25 +0900

    tab UI 수정

--- app/src/main/java/com/example/y/travel_diary/MainActivity.java
+++ app/src/main/java/com/example/y/travel_diary/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.y.travel_diary.Activities.AddNewBucket;
@@ -29,6 +30,7 @@ import com.example.y.travel_diary.Fragments.FragmentPlanner;
 public class MainActivity extends Activity {
     final public static String TRAVEL_PREF = "cur_travel";
     private SharedPreferences pref = null;
+    private int cur_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +46,14 @@ public class MainActivity extends Activity {
 
         Fragment fr;
         if ((pref != null) && pref.getInt("id", -1) != -1) {
+            cur_id = R.id.button_main;
+            ImageView mainImage = (ImageView) findViewById(R.id.button_main);
+            mainImage.setImageResource(R.drawable.mainimg);
             fr = new FragmentMain();
-        } else
+        } else {
+            cur_id = R.id.button_home;
             fr = new FragmentHome();
+        }
 
         FragmentManager fm = getFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
@@ -57,30 +64,63 @@ public class MainActivity extends Activity {
     // Change the fragment properly according to the Buttons clicked.
     public void selectFrag(View view) {
         Fragment fr = null;
+        int sid = view.getId();
+        ImageView imageView = null;
 
         if(pref.getInt("id", -1) == -1)
             return;
 
-        switch (view.getId()) {
+        if (sid != R.id.button_home)
+            imageView = (ImageView) findViewById(sid);
+
+        switch (sid) {
             case R.id.button_home:
                 fr = new FragmentHome();
                 break;
             case R.id.button_main:
+                imageView.setImageResource(R.drawable.mainimg);
                 fr = new FragmentMain();
                 break;
             case R.id.button_map:
+                imageView.setImageResource(R.drawable.mapimg);
                 fr = new FragmentMap();
                 break;
             case R.id.button_list:
+                imageView.setImageResource(R.drawable.listimg);
                 fr = new FragmentList();
                 break;
             case R.id.button_planner:
+                imageView.setImageResource(R.drawable.planimg);
                 fr = new FragmentPlanner();
                 break;
             case R.id.button_album:
+                imageView.setImageResource(R.drawable.albumimg);
                 fr = new FragmentAlbum();
                 break;
         }
+
+        if (cur_id != R.id.button_home) {
+            imageView = (ImageView) findViewById(cur_id);
+            switch (cur_id) {
+                case R.id.button_main:
+                    imageView.setImageResource(R.drawable.mainimg2);
+                    break;
+                case R.id.button_map:
+                    imageView.setImageResource(R.drawable.mapimg2);
+                    break;
+                case R.id.button_list:
+                    imageView.setImageResource(R.drawable.listimg2);
+                    break;
+                case R.id.button_planner:
+                    imageView.setImageResource(R.drawable.planimg2);
+                    break;
+                case R.id.button_album:
+                    imageView.setImageResource(R.drawable.albumimg2);
+                    break;
+            }
+        }
+
+        cur_id = sid;
 
         if (fr == null)
             return;
